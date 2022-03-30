c572647792aad901b64767ce63575734c0670e7c
NicolaMigliore <auster.mig@gmail.com>
2022-03-30 13:52:01 +0200

    add corrections in snack 5

--- snack5/main.js
+++ snack5/main.js
@@ -6,6 +6,10 @@ se è dispari inseriscilo nell’array. */
 //dichiaro un array vuoto
 const list_number = [];
 
+
+//dichiaro una variabile num in cui andranno i valori numerici inseriti dall'utente
+let num;
+
 //dichiaro una variabile booleana
 let bool =  true;
 
@@ -15,19 +19,21 @@ for(let i=0; i<6; i++){
 
     //dichiaro una variabile all'interno della quale inserisco il numero chiesto all'utente 
 
-    const num = parseInt(prompt("Inserisci un numero"));  
+    num = parseInt(prompt("Inserisci un numero"));  
 
     /*  console.log(i); */
+
+    //imposto la condizione che solo se il numero inserito dall'utente è dispari viene successivamente aggunto nel nostro array
 
     if(num % 2 == 0){
         bool = false; 
     } else {
     
-        list_number.push(num(i));
+        list_number.push(num);
     }
+    
 
 } 
 
-
-
+//stampo l'array in console che corrisonde alla lista dei numeri dispari inseriti dall'utente
 console.log(list_number);
