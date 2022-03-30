69f7a91b4329887f715050ec6a68fb6899257f49
NicolaMigliore <auster.mig@gmail.com>
2022-03-30 10:24:19 +0200

    add first snack

---
+++ snack1/main.js
@@ -0,0 +1,34 @@
+/*SNACK 1 
+
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+
+//dichiaro le variabili che conterranno i dati immessi da due utenti
+let user1;
+let user2;
+
+
+//inizio user 1 inserendo dati domandati all'utente
+user1 = parseInt(prompt("Inserisci un numero"));
+
+
+//inizio user2 inserendo dati domandati aell'utente
+user2 = parseInt(prompt("inserisci un numero"))
+
+
+//inserisco condizione per stabilire il numero maggiore e stamparlo in console
+if(user1>user2){
+
+    console.log(user1);
+
+
+    
+} else if (user1>user2){
+
+    console.log(user2);
+
+    
+} else {
+    console.log("i due numeri sono uguali")
+}
